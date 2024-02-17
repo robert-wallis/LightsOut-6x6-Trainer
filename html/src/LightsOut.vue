2bc91a94d40f65179f2e764aabd7bc095e89a142
Robert Wallis <smilingrob@gmail.com>
2024-02-17 00:40:12 -0800

    arrows svg for phase 2

--- html/src/LightsOut.vue
+++ html/src/LightsOut.vue
@@ -9,6 +9,8 @@ let cells = reactive(Array.from({ length: BOARD_WIDTH * BOARD_HEIGHT }, (_, i) =
     active: false,
     clickMe: false,
     phase2Clicked: false,
+    arrow1: false,
+    arrow2: false,
 })));
 
 let model = reactive({
@@ -122,6 +124,8 @@ function calculateHints() {
         } else {
             cell.clickMe = false;
             cell.phase2Clicked = false;
+            cell.arrow1 = false;
+            cell.arrow2 = false;
         }
     });
 
@@ -173,6 +177,8 @@ function calculateHints() {
             const cell = cells[row * BOARD_WIDTH + col];
             if (cell.active) {
                 anyActive = true;
+                cell.arrow1 = true;
+                cell.arrow2 = true;
                 let addIds = topRowCellsForBottomRowCell(cell.id);
                 idsToTick.push(...addIds);
             }
@@ -192,7 +198,8 @@ function calculateHints() {
         var anyPhase2Ticked = false;
         // finally tick the remaining ids
         for (let i = 0; i < remainingIds.length; i++) {
-            cells[remainingIds[i]].clickMe = true;
+            let cell = cells[remainingIds[i]];
+            cell.clickMe = true;
             anyPhase2Ticked = true;
         }
         if (anyPhase2Ticked) {
@@ -272,6 +279,28 @@ function topRowCellsForBottomRowCell(cellId) {
                 <div class="cellName">{{ cell.id + 1 }}</div>
                 <div class="clickMe" :class="{ active: model.showHints && cell.clickMe, phase2Clicked: model.showHints && cell.phase2Clicked }"></div>
             </div>
+            <svg class="arrows" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:serif="http://www.serif.com/" width="500" height="500" viewBox="0 0 378 378" version="1.1" xml:space="preserve" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:1.5;">
+              <path :class="{ phase2Arrow: cells[30].arrow1 }" id="arrow-1-1" d="M6.246,76.607l20.004,-22.607l6.083,29.568c-4.781,-8.262 -17.825,-11.743 -26.087,-6.961Z"/>
+              <path :class="{ phase2Arrow: cells[30].arrow1 }" d="M18.75,324.75c0,0 -11.956,-174.587 1.932,-249.88" style="fill:none;stroke:#000;stroke-width:9px;"/>
+              <path :class="{ phase2Arrow: cells[30].arrow2 }" id="arrow-1-3" d="M122.567,68.701l25.933,-15.451l-3.199,30.017c-2.042,-9.325 -13.409,-16.608 -22.734,-14.566Z"/>
+              <path :class="{ phase2Arrow: cells[30].arrow2 }" d="M40.5,324c0,0 27.228,-139.501 96.347,-252.563" style="fill:none;stroke:#000;stroke-width:9px;"/>
+              <path :class="{ phase2Arrow: cells[31].arrow1 }" id="arrow-2-4" d="M184.032,69.392l25.968,-15.392l-3.267,30.01c-2.021,-9.33 -13.371,-16.639 -22.701,-14.618Z"/>
+              <path :class="{ phase2Arrow: cells[31].arrow1 }" d="M94.5,324.75c0,0 33.979,-139.523 103.806,-252.589" style="fill:none;stroke:#000;stroke-width:9px;"/>
+              <path :class="{ phase2Arrow: cells[32].arrow1 }" id="arrow-3-1" d="M45.296,83.053l-4.796,-29.803l26.721,14.045c-9.421,-1.541 -20.383,6.338 -21.925,15.758Z"/>
+              <path :class="{ phase2Arrow: cells[32].arrow1 }" d="M145.5,324c0,0 -26.39,-154.311 -92.393,-253.211" style="fill:none;stroke:#000;stroke-width:9px;"/>
+              <path :class="{ phase2Arrow: cells[32].arrow2 }" id="arrow-3-5" d="M255.194,68.583l26.431,-14.583l-4.192,29.894c-1.732,-9.387 -12.851,-17.043 -22.239,-15.311Z"/>
+              <path :class="{ phase2Arrow: cells[32].arrow2 }" d="M168,324.75c0,0 19.7,-129.547 101.376,-252.959" style="fill:none;stroke:#000;stroke-width:9px;"/>
+              <path :class="{ phase2Arrow: cells[33].arrow1 }" id="arrow-4-2" d="M82.963,81.895l11.537,-27.895l15.394,25.967c-7.215,-6.251 -20.68,-5.287 -26.931,1.928Z"/>
+              <path :class="{ phase2Arrow: cells[33].arrow1 }" d="M207,324.75c0,0 -97.028,-123.958 -110.957,-249.205" style="fill:none;stroke:#000;stroke-width:9px;"/>
+              <path :class="{ phase2Arrow: cells[33].arrow2 }" id="arrow-4-6" d="M313.685,79.398l16.315,-25.398l10.529,28.291c-5.987,-7.434 -19.41,-8.881 -26.844,-2.893Z"/>
+              <path :class="{ phase2Arrow: cells[33].arrow2 }" d="M233.25,324.75c0,0 77.711,-128.751 94.436,-249.274" style="fill:none;stroke:#000;stroke-width:9px;"/>
+              <path :class="{ phase2Arrow: cells[34].arrow1 }" id="arrow-5-3" d="M160.139,82.933l8.611,-28.933l17.98,24.248c-7.819,-5.476 -21.114,-3.134 -26.591,4.685Z"/>
+              <path :class="{ phase2Arrow: cells[34].arrow1 }" d="M281.625,324.75c0,0 -86.225,-142.538 -109.128,-249.478" style="fill:none;stroke:#000;stroke-width:9px;"/>
+              <path :class="{ phase2Arrow: cells[35].arrow1 }" id="arrow-6-4" d="M221.476,81.046l11.774,-27.796l15.173,26.097c-7.162,-6.312 -20.635,-5.462 -26.947,1.699Z"/>
+              <path :class="{ phase2Arrow: cells[35].arrow1 }" d="M333.75,324c0,0 -87.126,-141.74 -99.14,-249.193" style="fill:none;stroke:#000;stroke-width:9px;"/>
+              <path :class="{ phase2Arrow: cells[35].arrow2 }" id="arrow-6-6" d="M350.604,82.152l10.896,-28.152l15.984,25.608c-7.356,-6.084 -20.796,-4.812 -26.88,2.544Z"/>
+              <path :class="{ phase2Arrow: cells[35].arrow2 }" d="M358.5,324.75c0,0 14.366,-133.215 5.035,-249.246" style="fill:none;stroke:#000;stroke-width:9px;"/>
+            </svg>
         </div>
     </div>
     <footer>Copyright &copy; 2024 <a href="https://smilingrob.com/">Robert Wallis</a></footer>
@@ -330,5 +359,16 @@ function topRowCellsForBottomRowCell(cellId) {
     }
     .cell .clickMe.phase2Clicked {
         background-color: rgba(0, 0, 0, 0.2);
+    }
+    .arrows {
+        position: absolute;
+        z-index: 10;
+        pointer-events: none;
+    }
+    .arrows path {
+        display: none;
+    }
+    .arrows path.phase2Arrow {
+        display: block;
     }
 </style>
